43080115b18c8f51477095ebc436120d2dfa09a2
Critteros <Critteros@users.noreply.github.com>
2022-05-21 01:10:44 +0200

    Basic backend structure, removed husky and lint-staged

--- app/components/organism/SpeedtestUI.tsx
+++ app/components/organism/SpeedtestUI.tsx
@@ -1,12 +1,25 @@
 import Gauge from '../atoms/Gauge';
 import StopRestartControls from '../molecules/StopRestartControls';
+import * as socketio from 'socket.io-client';
+import { useEffect } from 'react';
 
 type Props = {
   className?: string;
   id: string;
   colors?: string[];
 };
+
+// io do not accept for some reason type string|undefined
+const backendURL = process.env['NEXT_PUBLIC_BACKEND_URL'] ?? '';
+const socket = socketio.connect(backendURL);
+
 const SpeedtestUI = ({ className, id, colors }: Props) => {
+  useEffect(() => {
+    const backendUrl = process.env['NEXT_PUBLIC_BACKEND_URL'];
+    socket.emit('TEST', 'data');
+    console.log(socket.active);
+    console.log(backendUrl);
+  }, []);
   return (
     <div className={`flex flex-col items-center justify-center ${className ? className : ''}`}>
       <Gauge id={id} colors={colors} className="w-full sm:w-11/12" value={10} />
