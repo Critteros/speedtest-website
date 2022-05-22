c2b2ff350778ea021be99cf2b445d6c9c73ae321
Critteros <szymonkoziol3@gmail.com>
2022-05-23 01:23:24 +0200

    Added jest with first test

---
+++ app/components/atoms/Button.spec.tsx
@@ -0,0 +1,22 @@
+import Button from './Button';
+import button from './Button';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('Button', () => {
+  it('displays text', () => {
+    render(<Button type={'stop'} label="Stop" />);
+    const button = screen.getByText(/Stop/);
+    expect(button).toBeInTheDocument();
+  });
+
+  it('is clickable', async () => {
+    const mockCallback = jest.fn();
+    const user = userEvent.setup();
+
+    render(<Button type={'stop'} label="Stop" onClick={mockCallback} />);
+    const button = screen.getByRole('button');
+    await user.click(button);
+    expect(mockCallback).toBeCalled();
+  });
+});
